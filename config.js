7287912c528b932d0ad52e907a31bc796acb278e
Zac O <zorndorff@gmail.com>
2017-04-23 13:07:48 -0500

    update config

--- config.js
+++ config.js
@@ -60,33 +60,6 @@ config = {
             port: process.env.PORT
         }
     },
-    // ### Development **(default)**
-    testing: {
-        url: 'http://localhost:2368',
-        // #### Database
-        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-            },
-            debug: false
-        },
-        // #### Server
-        // Can be host & port (default), or socket
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
-        },
-        // #### Paths
-        // Specify where your content directory lives
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
-    },
-
     // **Developers only need to edit below here**
 
     // ### Testing
