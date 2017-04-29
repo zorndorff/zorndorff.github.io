85da5826597b0593b545c9384446ba527b6db207
Zac O <zorndorff@gmail.com>
2017-04-29 14:42:20 -0500

    add host config name

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ console.log('db path in config', path.join(__dirname, '/content/data/ghost.db'))
 config = {
     // ### Production
     production: {
-        url: 'http://0.0.0.0' ,
+        url: 'http://' + process.env.HOST,
         mail: {
             transport: 'SMTP',
             options: {
