ce1605df5521c46489ce63f4006ed1e34ff5f2ae
Zac O <zorndorff@gmail.com>
2018-03-11 12:09:49 -0500

    basic deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+BUILD_TIME = ${date}
+
+echo Generating new pages
+
+hugo
+
+echo Committing changes
+
+git add public/ 
+
+git commit public/ -m 'Content Update $BUILD_TIME'
+
+git push origin
+
