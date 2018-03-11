8e86672e722611d479052dac92ee1a76e5e0c555
Zac O <zorndorff@gmail.com>
2018-03-11 12:34:08 -0500

    clear docs before rebuilding

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,14 @@
 
 BUILD_TIME=`date`
 
-echo Generating new pages
+echo Clear old docs folder
+rm -rf docs
 
+echo Generating new pages
 hugo
 
 echo Committing changes
-
 git add docs/ 
-
 git commit docs/ -m "Content Update $BUILD_TIME"
-
 git push origin
 
