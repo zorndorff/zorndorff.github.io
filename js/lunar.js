10bf55994945838343a844e48b83700c393ae9ce
Zac O <zorndorff@gmail.com>
2020-06-19 08:44:57 -0500

    Content Update Fri Jun 19 08:44:57 CDT 2020

---
+++ js/lunar.js
@@ -0,0 +1,23 @@
+class Game {
+
+}
+class PhysObject{
+  constructor() {
+
+  }
+}
+
+class Game {
+  constructor () {
+
+  }
+}
+
+class Lander extends PhysObject {
+
+}
+
+window.addEventListener('load', (event) => {
+
+});
+
